3de49cef29330b93ce5a69aec57855d58f9de383
hoquocbao23 <hoquocbao6123@gmail.com>
2024-10-19 14:26:49 +0700

    update src code to see red alert

--- src/test/java/com/baonhq/mathutil/core/test/MathUtilityDDTTest.java
+++ src/test/java/com/baonhq/mathutil/core/test/MathUtilityDDTTest.java
@@ -39,7 +39,8 @@ class MathUtilityDDTTest {
                                 {1, 1},
                                 {2, 2},
                                 {3, 6},
-                                {4, 24}};
+                                {4, 24},
+                                {5, 120}};
     }
 
     @ParameterizedTest
